bcb606f3299dc9e9233a87fbacd24b7becf902f7
shenhuaqing <286732739@qq.com>
2024-04-30 15:30:11 +0800

    fix: change stubsRoot

--- stubs/main.ts
+++ stubs/main.ts
@@ -1,3 +1,3 @@
-import { getDirname } from '@poppinss/utils'
+import { joinToURL } from '@poppinss/utils'
 
-export const stubsRoot = getDirname(import.meta.url)
+export const stubsRoot = joinToURL(import.meta.url, 'stubs')
